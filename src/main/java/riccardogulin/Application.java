1931d60231e7d2739cdd612161ac0ef7670555a3
Riccardo Gulin <riccardo.gulin@gmail.com>
2026-06-11 12:06:41 +0200

    Adds Collectors & Comparators

--- src/main/java/riccardogulin/Application.java
+++ src/main/java/riccardogulin/Application.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 
 public class Application {
 
+
 	public static void main(String[] args) {
 		Supplier<List<User>> randomUserSupplier = () -> {
 			ArrayList<User> users = new ArrayList<>();
@@ -50,5 +51,54 @@ public class Application {
 		String namesSurnames2 = randomUsers.stream().map(user -> user.getName() + ", " + user.getSurname())
 				.collect(Collectors.joining(". "));
 		System.out.println(namesSurnames2);
+
+		// 5.a Somma delle età degli utenti (tramite reduce)
+		int sommaEtà = randomUsers.stream()
+				.map(user -> user.getAge())
+				.reduce(0, (partialSum, currentAge) -> partialSum + currentAge);
+		System.out.println(sommaEtà);
+
+		// 5.b Somma delle età degli utenti (tramite collect)
+		int sommaEtà2 = randomUsers.stream()
+				.collect(Collectors.summingInt(user -> user.getAge()));
+		System.out.println(sommaEtà2);
+
+		// 6. Calcola età media degli utenti
+		double average = randomUsers.stream().collect(Collectors.averagingInt(user -> user.getAge()));
+		System.out.println(average);
+
+		// 7. Raggruppiamo gli utenti per città e calcoliamo la media delle età per ogni città
+		Map<String, Double> averagePerCity = randomUsers.stream()
+				.collect(Collectors.groupingBy(user -> user.getCity(), Collectors.averagingInt(user -> user.getAge())));
+
+		averagePerCity.forEach((city, avg) -> System.out.println("City: " + city + ", average: " + avg));
+
+		// 8. Raggruppiamo gli utenti per città e calcoliamo statistiche tipo età media, somma delle età, età massima, età minima
+		Map<String, IntSummaryStatistics> statsPerCity = randomUsers.stream()
+				.collect(Collectors.groupingBy(user -> user.getCity(), Collectors.summarizingInt(user -> user.getAge())));
+
+		statsPerCity.forEach((city, stats) -> System.out.println("City: " + city + ", stats: " + stats));
+
+		System.out.println("---------------------------------- COMPARATORS --------------------------------------------------------");
+		// 1. Ordiniamo la lista utenti per età crescente
+		List<User> sortedByAge = randomUsers.stream().sorted(Comparator.comparing(user -> user.getAge())).toList();
+		sortedByAge.forEach(user -> System.out.println(user));
+
+		// Alternativa compatta con :: al posto delle lambda
+//		List<User> sortedByAge2 = randomUsers.stream().sorted(Comparator.comparing(User::getAge)).toList();
+//		sortedByAge.forEach(System.out::println);
+
+		// 2. Ordiniamo gli utenti per età decrescente
+		List<User> sortedByAgeDesc = randomUsers.stream().sorted(Comparator.comparing(User::getAge).reversed()).toList();
+		sortedByAgeDesc.forEach(System.out::println);
+		// Se uso reversed per invertire l'ordine non posso usare la sintassi semplice con le lambda (a meno di non fare un casting
+		// (User user) -> user.getAge(). Alternativa uso la sintassi :: User::getAge
+
+		System.out.println("-----------------------------");
+		// 3. Ordiniamo gli utenti per cognome ordine discendente
+		List<User> sortedBySurname = randomUsers.stream().sorted(Comparator.comparing(User::getSurname).reversed()).toList();
+		sortedBySurname.forEach(System.out::println);
+
+
 	}
 }
